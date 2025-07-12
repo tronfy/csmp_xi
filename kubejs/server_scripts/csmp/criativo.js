341ead662d984d853bf47d582a7c452a95c78db9
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2025-07-12 14:31:37 -0300

    fix hashes?

--- kubejs/server_scripts/csmp/criativo.js
+++ kubejs/server_scripts/csmp/criativo.js
@@ -4,6 +4,7 @@ ServerEvents.recipes(event => {
     event.replaceInput({output:'mekanism:module_gravitational_modulating_unit'},'minecraft:nether_star','kubejs:capicoin')
     event.remove({output:'justdirethings:upgrade_flight'})
     event.shapeless(Item.of('justdirethings:upgrade_flight', 1),['justdirethings:upgrade_blank','kubejs:capicoin'])
+    
     // outros
     event.shapeless(Item.of('mekanism:creative_energy_cube[mekanism:side_config={config:{energy:{eject:1b,side:{back:"output",bottom:"output",front:"output",left:"output",right:"output",top:"output"}},items:{side:{back:"input",bottom:"input",front:"output",left:"input",right:"input",top:"input"}}}},mekanism:energy={energy_containers:[L;9223372036854775807L]}]', 1),['mekanism:ultimate_energy_cube','kubejs:capicoin'])
     event.shapeless(Item.of('mekanism:creative_fluid_tank', 1),['mekanism:ultimate_fluid_tank','kubejs:capicoin'])
